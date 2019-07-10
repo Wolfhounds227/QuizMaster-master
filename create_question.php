2f58e704bf2754bbd40a251bdc94cc9d98e1ee2a
Samira Adam <gr6052up@metrostate.edu>
2019-07-09 23:52:55 -0500

    Update create_question.php

--- create_question.php
+++ create_question.php
@@ -26,6 +26,38 @@ $result = run_sql($sql);
 
 </head>
 
+<style>
+.input-group {
+	margin: 10px 0px 10px 0px;
+	width: 93%;
+	box-sizing:border-box;
+	height:auto;
+
+	
+}
+.input-group label {
+	display: block;
+	text-align: left;
+	margin: 3px;
+	word-wrap: break;
+	height:auto;
+	box-sizing:border-box;
+	
+	
+}
+.input-group input {
+	height: 30px;
+	width: 93%;
+	padding: 5px 10px;
+	font-size: 16px;
+	
+	border: 1px solid gray;
+	height:auto;
+	box-sizing:border-box;
+	
+}
+</style>
+
 <body>
 <form method='POST' action='create_question.php'>
 <select name='topic'>
@@ -36,13 +68,21 @@ while($row = $result->fetch_assoc()){
 	
 ?>
 </select>
+<div class="input-group">
 <label>Question: <input name="question"> </label>
+</div>
+<div class="input-group">
 <label>Choice a: <input name="choice_1"> </label>
 <label>Choice b: <input name="choice_2"> </label>
 <label>Choice c: <input name="choice_3"> </label>
 <label>Choice d: <input name="choice_4"> </label>
+</div><br>
+<div class="input-group">
 <label>Answer: <input name="answer"> </label>
 <label>Image Name: <input name="image_name"> </label>
+</div>
+<div class="input-group">
 <input type='submit' value='submit'>
+</div>
 </form>
 </body>
