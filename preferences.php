29ca2b45448ae887d8d7120acb66b21ce810b80d
Ryan Server <wolfhoundsdev@gmail.com>
2019-07-31 08:51:44 -0500

    Update preferences.php
    created a min-max for changing preferences. This ensures the application will not break and sets limits to the max question and topic length to the number of rows in the associated topic.

--- preferences.php
+++ preferences.php
@@ -8,11 +8,19 @@
 		run_sql($sql);
 	}
 	$sql = "select * from preferences";
-	$result = run_sql($sql);
-	
-	$num_topics = $result->fetch_assoc()['value'];
+	$result = run_sql($sql); 
+	$num_topics = $result->fetch_assoc()['value']; 
 	$num_of_questions = $result->fetch_assoc()['value'];
-
+	$sql2 = "select count(id) from topics;";
+	$result2 = run_sql($sql2); 
+	$max_topics = $result2->fetch_assoc()['count(id)'];
+	$sql3 = "select count(id) from questions;";
+	$result3 = run_sql($sql3); 
+	$max_questions = $result3->fetch_assoc()['count(id)'];
+?>
+<?php
+echo '<p>'.$max_topics.'</p>';
+echo '<p>'.$max_questions.'</p>';
 ?>
 <!DOCTYPE>
 <html>
@@ -72,9 +80,9 @@
 		?>	
 	<main>
 		<form action="" method="POST">	
-		<label>Number of topics per row: <input name="topics" type="number" value="<?php echo $num_topics;?>"/></label>
-		<label>Number of questions to show: <input name="questions_to_show" type="number" value="<?php echo $num_of_questions;?>"/></label>
-		<input type="submit" value="Login"/>
+		<label>Number of topics per row: <input name="topics" type="number" value="<?php echo $num_topics;?>" min="0" max="<?php echo $max_topics;?>"/></label>
+		<label>Number of questions to show: <input name="questions_to_show" type="number" value="<?php echo $num_of_questions;?>" min="0" max="<?php echo $max_questions;?>"/></label>
+		<input type="submit" value="submit"/>
 		</form>
 		<?php
 			if(isset($error)){
